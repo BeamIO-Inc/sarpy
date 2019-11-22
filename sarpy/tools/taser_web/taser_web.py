b977c47eda22e0217ac522c31234224e1fe32c05
Ryan LaClair <ryanlaclair@gmail.com>
2019-11-22 17:03:03 -0500

    Updated load_dotenv command

--- sarpy/tools/taser_web/taser_web.py
+++ sarpy/tools/taser_web/taser_web.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.getcwd(), '.env'))
 os.environ['FLASK_APP'] = 'run.py'
 os.environ['ATK_CONFIG'] = os.path.join(os.getcwd(), 'config.py')
 
